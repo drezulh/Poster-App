c96aac5b823ddc450cd777b9385e018452fc8d27
Gorkem Turan <turangorkem@gmail.com>
2023-02-23 11:25:16 +0300

    Add files via upload

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+import { MdPostAdd, MdMessage } from 'react-icons/md';
+
+import classes from './MainHeader.module.css';
+
+function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <h1 className={classes.logo}>
+        <MdMessage />
+        Poster App
+      </h1>
+      <p>
+        <Link to='/create-post' className={classes.button} >
+          <MdPostAdd size={18} />
+          New Post
+        </Link>
+      </p>
+    </header>
+  );
+}
+
+export default MainHeader;
